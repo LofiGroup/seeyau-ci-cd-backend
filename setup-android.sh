c1e323c0f08deedef6fa57fd0ee9345f1141abba
Darkhan <darkcanvas113v@gmail.com>
2022-10-31 07:18:47 +0900

    Мелкие исправления

--- setup-android.sh
+++ setup-android.sh
@@ -4,13 +4,15 @@ if [ ! -d "${ANDROID_HOME}" ]
 then
   apt-get install -y openjdk-8-jdk
 
-  export ANDROID_HOME=/usr/local/android-sdk
-  export PATH="$PATH:$ANDROID_HOME/cmdline-tools/latest"
+  echo "export ANDROID_HOME=/usr/local/android-sdk" >> ~/.bashrc
 
-  mkdir -p "$ANDROID_HOME"/cmdline-tools/latest
+  export CMD_TOOLS_ROOT=$PATH:$ANDROID_HOME/cmdline-tools/latest
+  echo "export PATH=$PATH:$CMD_TOOLS_ROOT/bin" >> ~/.bashrc
+
+  mkdir -p "$CMD_TOOLS_ROOT"
   curl -o commandlinetools.zip -L https://dl.google.com/android/repository/commandlinetools-linux-8512546_latest.zip
   unzip ./commandlinetools.zip
-  mv -v cmdline-tools/* "$ANDROID_HOME"/cmdline-tools/latest
+  mv -v cmdline-tools/* "$CMD_TOOLS_ROOT"
   rm cmdline-tools
   rm commandlinetools.zip
 
