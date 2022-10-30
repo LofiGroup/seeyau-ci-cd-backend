a02e767c548817572eb69643bef6697faa24b772
Darkhan <darkcanvas113v@gmail.com>
2022-10-31 07:00:59 +0900

    Мелкие исправления

--- setup-github-runner.sh
+++ setup-github-runner.sh
@@ -6,7 +6,7 @@ then
   mkdir actions-runner
   curl -o actions-runner-linux.tar.gz -L https://github.com/actions/runner/releases/download/v2.298.2/actions-runner-linux-x64-2.298.2.tar.gz
   tar xzf ./actions-runner-linux.tar.gz -C actions-runner
-  rm ./actions-runner-linux-x64-2.298.2.tar.gz
+  rm ./actions-runner-linux.tar.gz
 
   ./actions-runner/bin/installdependencies.sh
 fi
--- setup.sh
+++ setup.sh
@@ -4,8 +4,5 @@ apt-get update
 apt-get install -y curl
 apt-get install -y unzip
 
-chmod +x ./install-android.sh
-./install-android.sh
-
-chmod +x ./setup-github-runner.sh
-./setup-github-runner.sh
+sh install-android.sh
+sh setup-github-runner.sh
