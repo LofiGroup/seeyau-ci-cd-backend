91aea43f17e770fc963c3fd64a7630f32f17f133
Darkhan <darkcanvas113v@gmail.com>
2022-11-01 21:43:08 +0900

    Мелкие исправления

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sh wait_for_it.sh "${JENKINS_ADDRESS}" -t "${WAIT_FOR_IT_TIME}" -- echo "Jenkins is ready"
+sh /utility/wait_for_it.sh "${JENKINS_ADDRESS}" -t "${WAIT_FOR_IT_TIME}" -- echo "Jenkins is ready"
 
 # If no certificates are obtained, create dummy ones to start the nginx
 if [ ! -f "${KEYS_FOLDER}/privkey.pem" ]
