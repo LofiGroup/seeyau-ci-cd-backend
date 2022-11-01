672dc254e4e5ed14922fc2003770309225ea8b68
Darkhan <darkcanvas113v@gmail.com>
2022-11-01 19:15:17 +0900

    Мелкие исправления

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -4,7 +4,7 @@ chmod +x utility/wait_for_it.sh
 sh -c "utility/wait_for_it.sh -t \"${WAIT_FOR_IT_TIME}\" \"${JENKINS_ADDRESS}\" -- echo \"Jenkins is ready\""
 
 # If no certificates are obtained, create dummy ones to start the nginx
-if [ ! -d "${KEYS_FOLDER}" ]
+if [ ! -f "${KEYS_FOLDER}/privkey.pem" ]
 then mkdir -p "${KEYS_FOLDER}" &&
 echo "SSL certificates are not present creating dummy ones" &&
 openssl req -batch -newkey rsa:2048 -new -nodes -x509 -days 2 -keyout "${KEYS_FOLDER}"/privkey.pem -out "${KEYS_FOLDER}"/fullchain.pem
