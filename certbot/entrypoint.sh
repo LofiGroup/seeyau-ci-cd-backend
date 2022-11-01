f2b5fa53f9039a933e05f6ac512b4fe509f68a69
Darkhan <darkcanvas113v@gmail.com>
2022-11-01 20:04:21 +0900

    Мелкие исправления

--- certbot/entrypoint.sh
+++ certbot/entrypoint.sh
@@ -3,10 +3,10 @@
 chmod +x /utility/wait_for_it.sh
 sh -c "/utility/wait_for_it.sh -t \"${WAIT_FOR_IT_TIME}\" \"${NGINX_ADDRESS}\" -- echo \"Nginx is ready\""
 
-if [ ! -f "${KEYS_FOLDER}"/README ]
-then rm -r "${KEYS_FOLDER}" && echo "Deleted dummy certificate"
-fi
-
-certbot certonly --webroot -w /var/www/certbot --keep-until-expiring --email "${CERTBOT_EMAIL}" --agree-tos --no-eff-email -d "${DOMAIN_NAME}"
-
-trap exit TERM; while :; do certbot renew; sleep 12h & wait ${!}; done;
+#if [ ! -f "${KEYS_FOLDER}"/README ]
+#then rm -r "${KEYS_FOLDER}" && echo "Deleted dummy certificate"
+#fi
+#
+#certbot certonly --webroot -w /var/www/certbot --keep-until-expiring --email "${CERTBOT_EMAIL}" --agree-tos --no-eff-email -d "${DOMAIN_NAME}"
+#
+#trap exit TERM; while :; do certbot renew; sleep 12h & wait ${!}; done;
