01a17c69aa558de78db0e91e1258572cc64f2509
Darkhan <darkcanvas113v@gmail.com>
2022-11-01 20:56:54 +0900

    Мелкие исправления

--- certbot/entrypoint.sh
+++ certbot/entrypoint.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 
 chmod +x ./wait_for_it.sh
+echo "./wait_for_it.sh -t ${WAIT_FOR_IT_TIME} ${NGINX_ADDRESS} -- echo \"Nginx is ready\""
 sh -c "./wait_for_it.sh -t ${WAIT_FOR_IT_TIME} ${NGINX_ADDRESS} -- echo \"Nginx is ready\""
 
 sleep infinity
