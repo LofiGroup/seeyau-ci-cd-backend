e83b01598e68427883a74bfb8936d46704b6dcdb
Darkhan <darkcanvas113v@gmail.com>
2022-11-06 20:00:07 +0900

    Fix

--- backend/utils/file.py
+++ backend/utils/file.py
@@ -3,7 +3,7 @@ from fastapi import UploadFile
 
 def save_file(file_name: str, file: UploadFile) -> str:
     file_location = f"static/{file_name}"
-    with open(file_location, "wb+") as file_object:
+    with open(file_location, "wb") as file_object:
         file_object.write(file.file.read())
 
     return file_name
